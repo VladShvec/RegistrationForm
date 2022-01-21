dc04a0b2827cea80d9443e5de9cd904893f45817
VladShvec <vladshvec7@gmail.com>
2022-01-21 16:19:12 +0400

    Form validation on Client Side(Browser)

---
+++ js/form_check.js
@@ -0,0 +1,115 @@
+let name = document.getElementById('name');
+let surname = document.getElementById('surname');
+let email = document.getElementById('email');
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let button = document.getElementById('submit');
+let form = document.getElementById('form')
+
+//name func validation
+const nameFunc = (event) =>{
+    if(name.value === ''){
+        let warningName = document.getElementById('nameWar')
+        name.classList.add('red-border')
+        warningName.classList.add('d-block')
+        // console.log(warningName)
+    }else{
+        let warningName = document.getElementById('nameWar')
+        warningName.classList.remove('d-block')
+        name.classList.remove('red-border')
+    }
+}
+
+//surname func validation with connect name func
+const surnameFunc = (event) =>{
+    //check name field
+    if(name.value === '' ){
+        nameFunc(event);
+    }
+    if(surname.value === '') {
+        //surname check
+            let warningSurname = document.getElementById('surnameWar')
+            warningSurname.classList.add('d-block')
+            surname.classList.add('red-border')
+            // console.log(warningSurname)
+        } else {
+            let warningSurname = document.getElementById('surnameWar')
+            warningSurname.classList.remove('d-block')
+            surname.classList.remove('red-border')
+        }
+
+}
+
+const emailFunc = (event , email,form) =>{
+    //check email
+    if(name.value === ''){
+        nameFunc(event);
+    }
+    if(surname.value === '') {
+        surnameFunc(event);
+    }
+    if(email.value === ''){
+        let emailWar = document.getElementById('emailWar')
+        emailWar.classList.add('d-block')
+        email.classList.add('red-border')
+        const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        const address = document.forms[form].elements[email].value;
+        if(reg.test(address) === false) {
+            alert('Введите корректный e-mail');
+            return false;
+        }
+
+        // console.log(emailWar)
+    }else{
+        let emailWar = document.getElementById('emailWar')
+        emailWar.classList.remove('d-block')
+        email.classList.remove('red-border')
+    }
+
+}
+const userFunc = (event,email,form) =>{
+    if(name.value === ''){
+        nameFunc(event);
+    }
+    if(surname.value === '') {
+        surnameFunc(event);
+    }
+    if(email.value === ''){
+        emailFunc(event,email,form);
+    }
+    console.log(username.value)
+    if(username.value === '' || username.value.length < 3){
+        let userWar = document.getElementById('userWar')
+        userWar.classList.add('d-block')
+        username.classList.add('red-border')
+    }
+    if(username.value !== '' && username.value.length >= 4){
+        let userWar = document.getElementById('userWar')
+        userWar.classList.remove('d-block');
+        username.classList.remove('red-border')
+    }
+}
+
+const passwordFunc = (event,email,form) =>{
+    if(name.value === ''){
+        nameFunc(event);
+    }
+    if(surname.value === '') {
+        surnameFunc(event);
+    }
+    if(email.value === ''){
+        emailFunc(event,email,form);
+    }
+    if(username.value === '' || username.value.length < 3){
+        userFunc(event);
+    }
+    if(password.value === '' || password < 8){
+        let passwordWar = document.getElementById('passwordWar')
+        passwordWar.classList.add('d-block')
+        password.classList.add('red-border')
+    }else{
+        let passwordWar = document.getElementById('passwordWar')
+        passwordWar.classList.remove('d-block');
+        password.classList.remove('red-border')
+    }
+}
--- question.php
+++ question.php
@@ -2,28 +2,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login here</title>
+    <title>RegForm</title>
     <link rel="stylesheet" href="">
     <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="css/question.css">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 <div class="root">
     <div id='root_container' class="container pt-5">
-        <div class="left_button">
-            <button class="btn btn-info">
-                <a class="badge badge-info" href="index.html">Go Back</a>
-            </button>
+        <h1 class="text-center mb-4">Здравсвуйте, вы уже зарегистрироваться?</h1>
+        <div class="buttons_container container">
+            <button class="btn btn-success"><a href="login.php" class="text-dark">Да, залогиниться</a></button>
+            <button class="btn btn-warning"><a href="index.html" class="text-light">Нет, зарегистрироваться</a></button>
         </div>
-        <form action="login_check.php" method="post" class="w-50 container">
-            <input type="text" class="form-control mt-3"  name="username" placeholder="Please write your username">
-            <input type="text" class="form-control mt-3 mb-5"  name="password" placeholder="Please write your password">
-            <input type="submit" class="btn btn-success">
-        </form>
     </div>
 </div>
 </body>
